1860532e6852f774b834b926f95426502b1445a9
CristoVU Gee and Michael Bezdicek <cristovu@>
2016-08-31 20:30:04 -0700

    toggle from one to another using js classes

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,6 @@
 $(document).ready(function() {
-  $("h1").click(function(){
-    alert("This is a header.");
-  });
-
-  $("p").click(function(){
-      alert("This is an paragraph.");
-  });
-
-  $("img").click(function(){
-      alert("This is an image.");
+  $(".clickable").click(function() {
+    $("#initially-showing").toggle();
+    $("#initially-hidden").toggle();
   });
 });
